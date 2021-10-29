804e5a9c320049e6e12ee5516fe2f4ce55484a82
Yashaswini R <92724247+YashaswiniR25@users.noreply.github.com>
2021-10-29 11:00:03 +0530

    Add files via upload

---
+++ Mexican.java
@@ -0,0 +1,7 @@
+package com.Hierarchial;
+
+public class Mexican extends Restaurant{
+	 void severNachosandPasta() {
+		System.out.println("We serve only nachos and pasta");
+	}
+}
---
+++ Restaurant.java
@@ -0,0 +1,11 @@
+package com.Hierarchial;
+
+public class Restaurant {
+	public void useIngredients() {
+		System.out.println("You should use only mentioned ingredients");
+	}
+	
+	public void useVegetables() {
+		System.out.println("use only root vegetables");
+	}
+}
